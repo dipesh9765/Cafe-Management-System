c2cb9e3ace3926cad449677001cc5fcfdd3e1188
Dipesh <dipeshmarathe9765@gmail.com>
2024-07-21 17:27:27 +0530

    Initial commit

---
+++ com.inn.cafe/src/main/java/com/inn/cafe/constents/CafeConstants.java
@@ -0,0 +1,13 @@
+package com.inn.cafe.constents;
+
+public class CafeConstants {
+
+    public static final String SOMETHING_WENT_WRONG = "Something Went Wrong.";
+
+    public static final String INVAILD_DATA = "Invaild Data.";
+
+    public static final String UNAUTHORIZED_ACCESS = "Unauthorized access.";
+
+    public static final String STORE_LOCATION = "C:\\Users\\LAB3-PC19\\Documents\\Bills";
+
+}
